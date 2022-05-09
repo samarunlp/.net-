8e6e5bc466de0941d6d9120aec7e9458de1a440b
Samar <103960494+samarunlp@users.noreply.github.com>
2022-05-09 17:06:03 -0300

    Update Teoria10.cs

--- Teorias/Teoria10.cs
+++ Teorias/Teoria10.cs
@@ -1,12 +1,12 @@
 class Program
 {
     static void Main (string[] args){
-        Task t= PrintAsync();
+        Task<double> t2= PrintAsync2();
         for(int i=1;i <= 100; i++){
             Console.Write("-");
         }
-        Console.WriteLine();
-        t.Wait();
+        Console.Write($" \n Tiempo transcurrido: {t2.Result} \n"); //aca se hace la espera sincronica
+        
     }
 
     
@@ -27,5 +27,12 @@ class Program
         Console.Write($" \n Tiempo transcurrido: {mlseg} \n");
         //return t;     eliminamos esta linea por el await y no retorna t, retorna una tarea nueva q hace el operador
     }
-}
 
+    static async Task<double> PrintAsync2(){    //metodo print async modificado
+        Task t= new Task(ImprimirA);
+        DateTime inicio= DateTime.Now;
+        t.Start();
+        await t;
+        return (DateTime.Now - inicio).TotalMilliseconds;
+    }
+}
