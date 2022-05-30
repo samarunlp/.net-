f357843a2d6bc98fd7e81d9f88958ce56a1ec5b9
Samar <103960494+samarunlp@users.noreply.github.com>
2022-05-30 17:34:33 -0300

    Update Program.cs

--- Teorias/Teoria13/Program.cs
+++ Teorias/Teoria13/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
 using CalculoSimple;
 
-ILogger logger= new LoggerArchivo();
-Calculador calc=new Calculador(logger); //inyeccion de dependencia
-calc.Calcular(3);
+var servicios= new ServiceCollection();
+servicios.AddTransient<ICalculador,Calculador>();   //se registran los servicios y se construye el proveedor
+servicios.AddTransient<ILogger,Logger>();
+var proveedor= servicios.BuildServiceProvider();    //hasta acá
+var calc=proveedor.GetService<ICalculador>(); 
+calc?.Calcular(3);  //? evita posible null
+
+//de esta forma, la clase ProveedorServicios ya no es necesaria
