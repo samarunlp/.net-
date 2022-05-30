ea133f395b6ee774b169ad66c826a1de648792ee
Samar <103960494+samarunlp@users.noreply.github.com>
2022-05-30 17:35:10 -0300

    Update ILogger.cs

--- Teorias/Teoria13/ILogger.cs
+++ Teorias/Teoria13/ILogger.cs
@@ -1,5 +1,4 @@
 namespace CalculoSimple;
-public interface ILogger   //funcionaria como un enchufe, puedo reemplazar sin depender de algo
-{ 
+public interface ILogger{ //funcionaria como un enchufe, puedo reemplazar sin depender de algo
     void Log(string mensaje);
 }
