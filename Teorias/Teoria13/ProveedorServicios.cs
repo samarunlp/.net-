963e543dbf85ac3267dec255d8cfd0e6bc0328d2
Samar <103960494+samarunlp@users.noreply.github.com>
2022-05-30 17:38:07 -0300

    Update ProveedorServicios.cs

--- Teorias/Teoria13/ProveedorServicios.cs
+++ Teorias/Teoria13/ProveedorServicios.cs
@@ -1,10 +1,9 @@
-
 //no lo usamos pero lo tengo 
-
 
 namespace CalculoSimple;
 class ProveedorServicios{
     public ILogger GetLogger();
     => new Logger();
     public ICalculador GetCalculador();
-  
+    => new Calculador(this.GetLogger());
+}
