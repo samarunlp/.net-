d089ecd9b9d216447a465e13fb22722fde363cfd
Samar <103960494+samarunlp@users.noreply.github.com>
2022-08-30 17:38:25 -0300

    Update Practica2.cs

--- Prácticas/Practica2.cs
+++ Prácticas/Practica2.cs
@@ -70,3 +70,41 @@ st = (char)65;   //error pq no se puede convertir implicitamente un char a un st
 st = (string)65;  //error pq no se puede convertir un int a un string
 st = 47.89.ToString();
 
+
+/*
+8. Escribir un programa que reciba una lista de nombres como parámetro e imprima por consola un
+saludo personalizado para cada uno de ellos.
+a) utilizando la sentencia for
+b) utilizando la sentencia foreach , es usado cuando quiero recorrer todo el vector */
+
+string[] vector= new string [3] {"Samar", "Rocio", "Emanuel"}; //o puede ir sin el new string y tamaño.
+
+//a) utilizando la sentencia for
+for (int i=0;i < vector.Length;i++){
+           Console.WriteLine("Hola "+ vector[i]);
+           }
+//b) utilizando la sentencia foreach
+foreach (string st in vector){              //st que va a asignarse con cada uno de los elementos de la colección
+   Console.WriteLine("Hola "+st);
+           }
+/*
+9. Investigar acerca de la clase StringBuilder del espacio de nombre System.Text ¿En qué
+circunstancias es preferible utilizar StringBuilder en lugar de utilizar string? Implementar
+un caso de ejemplo en el que el rendimiento sea claramente superior utilizando
+StringBuilder en lugar de string y otro en el que no.
+> Es preferible utilizar StringBuilder en vez de string en el caso de que a lo largo de la ejecucion del programa, querramos modificar la cadena.
+*/
+
+using System.Text;
+
+StringBuilder cad;
+cad = new StringBuilder ("casa");
+Console.WriteLine(cad);
+cad[0] = 'C';
+Console.WriteLine(cad);
+
+
+/*
+10. Investigar sobre el tipo DateTime y usarlo para medir el tiempo de ejecución de los algoritmos
+implementados en el ejercicio anterior. */
+
