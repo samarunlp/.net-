f82320e2cfac6c8c51b9f9a2c0a1f01586ebe7e3
Samar <103960494+samarunlp@users.noreply.github.com>
2022-09-20 21:29:39 -0300

    Update Practica2.cs

--- Prácticas/Practica2.cs
+++ Prácticas/Practica2.cs
@@ -174,3 +174,29 @@ class Program
 
   }
 }
+/*
+14. Implementar un programa que muestre todos los números primos entre 1 y un número natural
+dado (pasado al programa como argumento por la línea de comandos). Definir el método bool
+EsPrimo(int n) que devuelve true sólo si n es primo. Esta función debe comprobar si n es
+divisible por algún número entero entre 2 y la raíz cuadrada de n. (Nota: Math.Sqrt(d)
+devuelve la raíz cuadrada de d)  */
+int  numDado = Convertir . ToInt32 ( argumentos [ 0 ]); // como es un string tengo q pasarlo a int
+consola _ WriteLine ( " Numeros primos del 1 al "  +  numDado );
+for ( int  i  =  1 ; i  <=  numDado ; i ++ )
+// para (int i = 1; i < 11; i++)
+{
+   si ( EsPrimo ( yo ))
+   {
+       consola _ WriteLine ( i );
+   }
+}
+
+ booleano estático EsPrimo  ( int n ) 
+{
+    bool  EsP = falso ;
+    si ( ( n  %  2  !=  0 ) && ( Matemáticas . Sqrt ( n ) %  2  !=  0 ) )
+    {
+        ESP = verdadero ;
+    }
+    volver  ESP ;
+}
