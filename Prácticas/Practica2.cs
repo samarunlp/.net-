4584af1cce36fabb3591a9f5768c5a2255d0d43f
Samar <103960494+samarunlp@users.noreply.github.com>
2022-09-20 21:55:48 -0300

    Update Practica2.cs

--- Prácticas/Practica2.cs
+++ Prácticas/Practica2.cs
@@ -200,3 +200,19 @@ for ( int  i  =  1 ; i  <=  numDado ; i ++ )
     }
     volver  ESP ;
 }
+
+*//*
+Escribir una función (método int Fib(int n)) que calcule el término n de la serie de
+Fibonacci. */
+Fib(n) = 1, si n <=2
+Fib(n) = Fib(n-1) + Fib(n-2), si n > 2
+*/
+static int Fib(int n)
+{
+    if(n<=2) return 1;
+    else return Fib(n-1) + Fib(n-2);
+}
+
+int i=1;
+Console.WriteLine(Fib(i));
+Console.WriteLine(Fib(i+2));
